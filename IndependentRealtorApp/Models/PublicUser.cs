3d4e74917398cc0375a97fb162799c8beafdae25
Hamberfim <adhamlin@gmail.com>
2024-04-05 17:37:38 -0500

    annotate PublicUser model

--- IndependentRealtorApp/Models/PublicUser.cs
+++ IndependentRealtorApp/Models/PublicUser.cs
@@ -4,7 +4,9 @@
  * NOTES: We will use a mix of conventions, annotations and fluent API to config DB context and db models
  */
 
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IndependentRealtorApp.Models
 {
@@ -15,10 +17,28 @@ namespace IndependentRealtorApp.Models
 
         [Key]
         public int PublicUserId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your last name")]
         public string LastName { get; set; } = string.Empty;
+
+        // MAYBE TODO: REGEX and/or check if email is already in DB
+        [Required(ErrorMessage = "Please Enter an email address. Your email address will also be your user name.")]
+        [Display(Name = "Email/User Name")]
         public string PublicUserNameEmail { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "Please Enter a password.")]
+        [Compare("ConfirmPassword")]
+        [StringLength(25, ErrorMessage = "Password is limited to a maximum of 25 characters")]
         public string PublicUserPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [Display(Name = "Confirm Password")]
+        [NotMapped]
+        public string ConfirmPassword { get; set; } = string.Empty;
 
 
         // from the other model (one PublicUser to many PropertyItems)
