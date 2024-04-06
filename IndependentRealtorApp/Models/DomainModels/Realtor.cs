3344085a379b7d517860cc3460ae7fa92201d624
Hamberfim <adhamlin@gmail.com>
2024-04-06 09:33:49 -0500

    Rethinking implementations model structure

---
+++ IndependentRealtorApp/Models/DomainModels/Realtor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace IndependentRealtorApp.Models.DomainModels
+{
+    public class Realtor
+    {
+        // when I construct the Realtor object, I want to initialize the Properties collection
+        public Realtor() => Properties = new HashSet<Property>();  // HashSet is a collection type that doesn't allow duplicates
+
+        public int RealtorId { get; set; }
+
+        [Display(Name = "First Name")]
+        [StringLength(125, ErrorMessage = "First name must be 125 characters or less")]
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Display(Name = "Last Name")]
+        [StringLength(125, ErrorMessage = "Last name must be 125 characters or less")]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName}";  // read-only property
+
+        public ICollection<Property> Properties { get; set; }
+    }
+}
---
+++ IndependentRealtorApp/Models/DomainModels/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace IndependentRealtorApp.Models.DomainModels
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Display(Name = "First Name")]
+        [StringLength(125, ErrorMessage = "First name must be 125 characters or less")]
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Display(Name = "Last Name")]
+        [StringLength(125, ErrorMessage = "Last name must be 125 characters or less")]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName}";  // read-only property
+
+        public ICollection<Property>? Properties { get; set; }
+    }
+}
