34968f7c498139355a20974e93a4c5d01b002767
Hamberfim <adhamlin@gmail.com>
2024-04-23 13:28:43 -0500

    Migrations and update to DB

---
+++ IndependentRealtorApp/Migrations/20240423182809_IdentityEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IndependentRealtorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "imageUrl",
+                table: "Properties",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Properties",
+                newName: "imageUrl");
+        }
+    }
+}
