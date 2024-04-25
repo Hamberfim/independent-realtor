feee7c460a57de241d231453130344981555f77a
Hamberfim <adhamlin@gmail.com>
2024-04-25 16:26:16 -0500

    Clean Migration, Updat DB, merge conflct fixes.

---
+++ IndependentRealtorApp/Migrations/20240425210717_FreshMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IndependentRealtorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FreshMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
