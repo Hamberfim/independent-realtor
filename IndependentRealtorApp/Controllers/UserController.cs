416acc9e8af4ee41bc7f5925cecf1003563bc2c2
BrodyBoell <143550750+BrodyBoell@users.noreply.github.com>
2024-04-25 15:41:58 -0500

    Update UserController.cs

--- IndependentRealtorApp/Controllers/UserController.cs
+++ IndependentRealtorApp/Controllers/UserController.cs
@@ -1,13 +1,16 @@
-﻿using IndependentRealtorApp.Models.DomainModels;
+using IndependentRealtorApp.Models.DomainModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IndependentRealtorApp.Controllers
 {
     public class UserController : Controller
     {
-        // inject IUser instead of DbContext
-        private IUser _user;
-        public UserController(IUser user) => _user = user;
+        private readonly IUser _user;
+
+        public UserController(IUser user)
+        {
+            _user = user;
+        }
 
         public IActionResult Index()
         {
@@ -15,7 +18,83 @@ namespace IndependentRealtorApp.Controllers
             return View(users);
         }
 
+        public IActionResult Add()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _user.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _user.GetUserById(id.Value);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _user.Update(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _user.GetUserById(id.Value);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var user = _user.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _user.Delete(user);
             return RedirectToAction(nameof(Index));
         }
     }
