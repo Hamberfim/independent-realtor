6fc877a58115ba46bf5b5e75b9364f3b5f118891
Hamberfim <adhamlin@gmail.com>
2024-04-26 09:22:07 -0500

    requirements

---
+++ IndependentRealtorApp/Areas/Admin/TagHelpers/DeleteButton.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace IndependentRealtorApp.Areas.Admin.TagHelpers
+{
+    // TODO: FIX THIS, Doesn't seem to be working
+    [HtmlTargetElement("button", Attributes = "id=delete,name=delete,asp-controller=Property,asp-action=Delete")]
+    public class DeleteButton : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", "btn btn-danger btn-sm");
+        }
+    }
+}
